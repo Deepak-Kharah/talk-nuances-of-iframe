c110cd8d0379dbdf761498039e02f5178ef0e452
Deepak Kharah <42672761+Deepak-Kharah@users.noreply.github.com>
2024-11-19 23:11:21 +0530

    feat: automate copyright year

--- primary-app/src/components/Footer/Footer.tsx
+++ primary-app/src/components/Footer/Footer.tsx
@@ -1,9 +1,14 @@
 import styles from "./Footer.module.css";
 
 export function Footer() {
+  const currentYear = new Date().getFullYear();
   return (
     <footer className={styles.footer}>
-      <p>&copy; 2024 Deepak Kharah. All rights reserved.</p>
+      <p>
+        &copy; 2024-{currentYear}{" "}
+        <a href="https://deepakkharah.com">Deepak Kharah</a>. All rights
+        reserved.
+      </p>
     </footer>
   );
 }
